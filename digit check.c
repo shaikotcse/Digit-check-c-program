e49e6059596d28f4c204dfe38f207d9c5dd04cc8
Manirul Islam Shaikot <32617352+shaikotcse@users.noreply.github.com>
2017-11-16 03:30:03 +0600

    Add files via upload
    check digit or not

---
+++ digit check.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    
+    printf("enter your character\n");
+    
+    scanf("%c",&ch);
+    
+  if(ch>=0 & ch>10){
+        
+        printf("u enter number of digit\n");
+    }
+    
+    else
+    printf("not a number");
+    
+    
+    
+    return 0;
+}
+
+
+
